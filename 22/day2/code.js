f2a10ab893786657aa1b02a506000d43297a260b
Steve Goode <steve.goode@nearform.com>
2022-12-02 11:24:49 +0000

    Made code more scaleable and caclulate, rather than just pattern matching

--- 22/day2/code.js
+++ 22/day2/code.js
@@ -1,23 +1,82 @@
 const fs = require("fs");
 
-const options = {
-  "A X": { part1: 1 + 3, part2: 0 + 3 },
-  "A Y": { part1: 2 + 6, part2: 3 + 1 },
-  "A Z": { part1: 3 + 0, part2: 6 + 2 },
-  "B X": { part1: 1 + 0, part2: 0 + 1 },
-  "B Y": { part1: 2 + 3, part2: 3 + 2 },
-  "B Z": { part1: 3 + 6, part2: 6 + 3 },
-  "C X": { part1: 1 + 6, part2: 0 + 2 },
-  "C Y": { part1: 2 + 0, part2: 3 + 3 },
-  "C Z": { part1: 3 + 3, part2: 6 + 1 },
+/**
+ * Configure the game rules
+ */
+const selections = {
+  ROCK: { points: 1, beats: null, loses: null },
+  PAPER: { points: 2, beats: null, loses: null },
+  SCISSORS: { points: 3, beats: null, loses: null },
 };
 
-let overall_score = { part1: 0, part2: 0 };
+selections.ROCK.beats = selections.SCISSORS;
+selections.ROCK.loses = selections.PAPER;
+
+selections.PAPER.beats = selections.ROCK;
+selections.PAPER.loses = selections.SCISSORS;
+
+selections.SCISSORS.beats = selections.PAPER;
+selections.SCISSORS.loses = selections.ROCK;
+
+/**
+ * Define game points
+ */
+const WIN_SCORE = 6;
+const DRAW_SCORE = 3;
+const LOSE_SCORE = 0;
+
+/**
+ * Define common conversions
+ */
+const their_options = {
+  A: selections.ROCK,
+  B: selections.PAPER,
+  C: selections.SCISSORS,
+};
+
+/**
+ * Do the calculations!
+ */
+let part1_score = 0;
+let part2_score = 0;
 
 const lines = fs.readFileSync("./input.txt").toString().split("\n");
 for (const line of lines) {
-  overall_score.part1 += options[line].part1;
-  overall_score.part2 += options[line].part2;
+  const [theirs, ours] = line.split(" ");
+  part1_score += calculatePart1(theirs, ours);
+  part2_score += calculatePart2(theirs, ours);
+}
+console.log("Part 1:", part1_score);
+console.log("Part 2:", part2_score);
+
+function calculatePart1(theirs, ours) {
+  const our_options = {
+    X: selections.ROCK,
+    Y: selections.PAPER,
+    Z: selections.SCISSORS,
+  };
+
+  const their_selection = their_options[theirs];
+  const our_selection = our_options[ours];
+
+  let outcome_points =
+    our_selection.beats == their_selection
+      ? WIN_SCORE
+      : their_selection.beats == our_selection
+      ? LOSE_SCORE
+      : DRAW_SCORE;
+
+  return our_selection.points + outcome_points;
 }
 
-console.log(overall_score);
+function calculatePart2(theirs, ours) {
+  const their_selection = their_options[theirs];
+  switch (ours) {
+    case "X": // Lose
+      return LOSE_SCORE + their_selection.beats.points;
+    case "Y": // Draw
+      return DRAW_SCORE + their_selection.points;
+    case "Z": // Win
+      return WIN_SCORE + their_selection.loses.points;
+  }
+}
