15f2a56f62f76f21376e5f5be029c374cbf6e7d0
Steve Goode <steve.goode@nearform.com>
2022-12-06 11:45:20 +0000

    Use slice to set the window

--- 22/day6/code.js
+++ 22/day6/code.js
@@ -1,29 +1,23 @@
 const fs = require("fs");
 
 let chars = fs.readFileSync("./input.txt").toString().split("");
+const packet_width = 4;
+const message_width = 14;
 
-let packet_chars = [];
-let message_chars = [];
 let first_part_answer = 0;
 let second_part_answer = 0;
+
 for (i = 0; i < chars.length; i++) {
   if (!first_part_answer) {
-    packet_chars.push(chars[i]);
-    if (new Set(packet_chars).size === 4) {
-      first_part_answer = i + 1;
+    if (new Set(chars.slice(i - packet_width, i)).size === packet_width) {
+      first_part_answer = i;
     }
-    if (packet_chars.length > 3) packet_chars.shift();
   }
 
-  if (!second_part_answer) {
-    message_chars.push(chars[i]);
-    if (new Set(message_chars).size === 14) {
-      second_part_answer = i + 1;
-    }
-    if (message_chars.length > 13) message_chars.shift();
+  if (new Set(chars.slice(i - message_width, i)).size === message_width) {
+    second_part_answer = i;
+    break;
   }
-
-  if (first_part_answer && second_part_answer) break; // We've got both answers, let's stop
 }
 
 console.log("First part:", first_part_answer);
