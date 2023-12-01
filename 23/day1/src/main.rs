307b0a543bc48cd5aa31c86976ced9c48c2c91d1
Steve Goode <stevei.goode@nearform.com>
2023-12-01 20:38:20 +0000

    Added step 2

--- 23/day1/src/main.rs
+++ 23/day1/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+use fancy_regex::Regex;
+
 fn part1() -> i32 {
     fs::read_to_string("input.txt")
         .unwrap()
@@ -20,6 +22,55 @@ fn part1() -> i32 {
         .sum()
 }
 
+fn convert(m: &str) -> u32 {
+    match m {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => m.chars().last().unwrap().to_digit(10).unwrap(),
+    }
+}
+
+fn part2() -> i32 {
+    let re = Regex::new(r"(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))").unwrap();
+    fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let caps = re.captures_iter(line);
+            if caps.count() == 0 {
+                return 0;
+            }
+            let first_match = convert(
+                re.captures_iter(line)
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .get(1)
+                    .unwrap()
+                    .as_str(),
+            );
+            let last_match = convert(
+                re.captures_iter(line)
+                    .last()
+                    .unwrap()
+                    .unwrap()
+                    .get(1)
+                    .unwrap()
+                    .as_str(),
+            );
+            return (first_match * 10 + last_match) as i32;
+        })
+        .sum()
+}
+
 fn main() {
     dbg!(part1());
+    dbg!(part2());
 }
